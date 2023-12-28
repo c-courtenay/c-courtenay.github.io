6434443f00545ac1fb05dd197c8230cce635c390
c-courtenay <155095178+c-courtenay@users.noreply.github.com>
2023-12-28 17:37:41 +0000

    New files added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Location search coming soon!');
+    // Here you can add functionality to prompt for location access
+    // Or redirect to another section/page that handles the location input
+});
+
